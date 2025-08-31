5e8b5976246018cd8c0a59a89356142121426005
Adrian <adrndrj@gmail.com>
2025-08-31 02:00:54 +0200

    added Bootstrap and BrowserRouter to prepare for different page routes

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { TweetList } from './components/TweetList'
+import { Server } from "socket.io";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+// Bootstrap Components
+import './index.css'; 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
